a111bd74afd91cb71437645a4d1fda4eaadf2e9b
yqs <yanqingsong@pansoft.com>
2026-07-02 11:35:24 +0800

    sql精简

--- docs/sql/migrations/20260702_001_datasource_db_name.down.sql
+++
@@ -1,2 +0,0 @@
--- 回滚：删除 db_name 字段
-ALTER TABLE cmx_sys_datasource DROP COLUMN IF EXISTS db_name;
--- docs/sql/migrations/20260702_001_datasource_db_name.up.sql
+++
@@ -1,5 +0,0 @@
--- 数据源表新增 db_name 字段（数据源显示名称，便于运维识别）
-
-ALTER TABLE cmx_sys_datasource ADD COLUMN IF NOT EXISTS db_name VARCHAR(128);
-
-COMMENT ON COLUMN cmx_sys_datasource.db_name IS '数据源名称（便于识别的显示名称）';
